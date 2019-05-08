6e165a60d2f9f6e6bfacf03a29270fd7c44ee654
df4u1t <50423443+df4u1t@users.noreply.github.com>
2019-05-08 15:27:03 -0500

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,35 +1,33 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+#Created by DF4U1T
+#Prompts for IP/Subnet to scan, interface to use, and will either create a new directory or use an existing directory for nmap output
 echo -e "What IP or Subnet would you like to scan?\n"
 	read IP
 echo -e "What interface would you like to use?\n"
 	read INT
 echo -e "What directory would you like to place the output in? (ex. /root/foo/bar/nmap/\n"
-	read PATH
+	read DIR
 echo -e "Would you like to create this directory? (Y/n)\n"
 	read ANSWER
 
 if [ $ANSWER = "Y" -o $ANSWER = "y" ]
 then
-	/usr/bin/mkdir -p $PATH
-	/usr/bin/masscan -p1-65535 -pU:1-65535 $IP --rate=1000 -e $INT > ports.txt
-	ports=$(/usr/bin/cat ports.txt | /usr/bin/awk -F " " '{print $4}' | /usr/bin/awk -F "/" '{print $1}' | /usr/bin/sort -n | /usr/bin/tr '\n' ',' | /usr/bin/sed 's/,$//')
-	/usr/bin/nmap -Pn -sV -sC -p$ports  -oA "$PATH"full-tcp $IP
-	/usr/bin/nmap -Pn -sU -sV -sC -p$ports -oA "$PATH"full-udp $IP
+	mkdir -p $DIR
+	masscan -p1-65535 -pU:1-65535 $IP --rate=10000 -e $INT > "$DIR"ports.txt
+	ports=$(cat "$DIR"ports.txt | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
+	nmap -Pn -sV -sC -p$ports  -oA "$DIR"full-tcp $IP
+	nmap -Pn -sU -sV -sC -p$ports -oA "$DIR"full-udp $IP
+	rm "$DIR"ports.txt
 
 elif [ $ANSWER = "N" -o $ANSWER = "n" ]
 then
-	/usr/bin/masscan -p1-65535 -pU:1-65535 $IP --rate=1000 -e $INT > ports.txt
-	ports=$(/usr/bin/cat ports.txt | /usr/bin/awk -F " " '{print $4}' | /usr/bin/awk -F "/" '{print $1}' | /usr/bin/sort -n | /usr/bin/tr '\n' ',' | /usr/bin/sed 's/,$//')
-	/usr/bin/nmap -Pn -sV -sC -p$ports  -oA "$PATH"full-tcp $IP
-	/usr/bin/nmap -Pn -sU -sV -sC -p$ports -oA "$PATH"full-udp $IP
+	masscan -p1-65535 -pU:1-65535 $IP --rate=1000 -e $INT > "$DIR"ports.txt
+	ports=$(cat "$DIR"ports.txt | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
+	nmap -Pn -sV -sC -p$ports  -oA "$DIR"full-tcp $IP
+	nmap -Pn -sU -sV -sC -p$ports -oA "$DIR"full-udp $IP
+	rm "$DIR"ports.txt
 
 else
 	echo ERROR: invalid option: exiting 1>&2
 	exit 1
 fi
-
-#/usr/bin/masscan -p1-65535 -pU:1-65535 $IP --rate=1000 -e $INT > ports.txt
-#ports=$(/usr/bin/cat ports.txt | /usr/bin/awk -F " " '{print $4}' | /usr/bin/awk -F "/" '{print $1}' | /usr/bin/sort -n | /usr/bin/tr '\n' ',' | /usr/bin/sed 's/,$//')
-#/usr/bin/nmap -Pn -sV -sC -p$ports  -oA "$PATH"full-tcp $IP
-#/usr/bin/nmap -Pn -sU -sV -sC -p$ports -oA "$PATH"full-udp $IP
